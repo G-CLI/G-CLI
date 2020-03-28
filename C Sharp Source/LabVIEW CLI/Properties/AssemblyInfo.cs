d00d6e1142096c553077cf634a08d90be95ba068
James McNally <james@wiresmithtech.com>
2020-03-28 07:20:13 +0000

    Bumped C# Project version

--- C Sharp Source/LabVIEW CLI/Properties/AssemblyInfo.cs
+++ C Sharp Source/LabVIEW CLI/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Wiresmith Technology")]
 [assembly: AssemblyProduct("G CLI")]
-[assembly: AssemblyCopyright("Copyright © LabVIEW CLI Contributors 2015-2019")]
+[assembly: AssemblyCopyright("Copyright © LabVIEW CLI Contributors 2015-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
