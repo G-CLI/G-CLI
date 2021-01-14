ebf9c6dd3ec7923810b25127df837ae28668c4b0
James McNally <james@wiresmithtech.com>
2021-01-14 21:50:26 +0000

    Add Process Monitor Logic
    Added thread for the process monitoring and detection of processes.
    
    Actually can simplify to include the launch to make it a "MonitoredProcess" class.

--- rust-proxy/src/labview/process.rs
+++ rust-proxy/src/labview/process.rs
@@ -1,24 +1,30 @@
 use std::collections::HashMap;
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
 use std::process::Command;
 use sysinfo::{System, SystemExt, ProcessExt};
+use log::{info, debug};
 
 
-
+type Pid = i32;
 
 const COMMAND: &str = "firefox";
 
 /// Launches the LabVIEW process.
 /// Returns the process ID.
-pub fn launch() -> u32 {
+pub fn launch() -> Pid {
 
     let output = Command::new(COMMAND)
         .spawn().expect("Failed to launch");
 
-    output.id()
+    debug!("Process launched with PID {}", output.id());
+
+    output.id() as i32
 }
 
 /// Returns a list of all instances running of LabVIEW
-pub fn find_instances() -> HashMap<i32, String> {
+pub fn find_instances() -> HashMap<Pid, String> {
 
     let sys = System::new_all();
     let mut processes = HashMap::new();
@@ -30,4 +36,127 @@ pub fn find_instances() -> HashMap<i32, String> {
     }
 
     processes
+}
+
+pub struct ProcessMonitor {
+    stop_channel: mpsc::Sender<bool>,
+    process_lost_channel: mpsc::Receiver<bool>
+}
+
+impl ProcessMonitor {
+
+    pub fn start(original_pid: Pid) -> Self {
+
+        //setup a channel for passing stop messages/
+        let (stop_tx, stop_rx) = mpsc::channel();
+
+        //setup a channel for sending a notification that the process is lost.
+        let (lost_tx, lost_rx) = mpsc::channel();
+
+        thread::spawn(move || {
+
+            let mut current_pid = original_pid;
+
+            while stop_rx.try_recv().is_err() {
+                let matching_processes = find_instances();
+
+                if let Some(id) = find_process(&matching_processes, current_pid) {
+
+                    if id != current_pid {
+                        current_pid = id;
+                        info!("Process lost + found at PID {}", id);
+                    }
+
+                }
+                else {
+                    info!("Process Lost");
+                    //send the warning - don't care if this errors.
+                    let _ = lost_tx.send(true);
+                    break;
+                }
+
+                thread::sleep(Duration::from_millis(100));
+            }
+
+        });
+
+
+
+        Self {
+            stop_channel: stop_tx,
+            process_lost_channel: lost_rx
+        }
+    }
+
+
+    pub fn stop(&self) {
+        self.stop_channel.send(true);
+    }
+
+    pub fn check_process_stopped(&self) -> bool {
+        self.process_lost_channel.try_recv().is_ok()
+    }
+
+}
+
+/// Find the process in the list.
+/// Return Some(pid) if a valid process is found.
+/// Return None if no process matches.
+fn find_process(processes: &HashMap<Pid, String>, original_id: Pid) -> Option<Pid> {
+    
+    let original_found = processes.contains_key(&original_id);
+
+    if original_found {
+        Some(original_id)
+    }
+    else {
+
+        // Returns None if no others, or some if they do.
+        processes.keys().next()
+            //map to make it owned.
+            .map(|id| id.clone())
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    fn test_process_list() -> HashMap<Pid, String> {
+
+        let mut processes =  HashMap::new();
+        processes.insert(1, String::from("Process"));
+        return processes;
+    }
+
+    #[test]
+    fn find_processes_same_process() {
+
+        let processes = test_process_list();
+
+        assert_eq!(find_process(&processes, 1), Some(1));
+    }
+
+    
+    #[test]
+    fn find_processes_new_process() {
+
+        let processes = test_process_list();
+
+        assert_eq!(find_process(&processes, 2), Some(1));
+    }
+
+    #[test]
+    fn find_processes_none() {
+
+        let mut processes = test_process_list();
+        processes.remove(&1); //remove the only entry.
+
+        assert_eq!(find_process(&processes, 1), None);
+    }
+
+
+
 }
--- rust-proxy/src/main.rs
+++ rust-proxy/src/main.rs
@@ -1,11 +1,30 @@
 mod labview;
 
+use log::debug;
+use simple_logger::SimpleLogger;
+
 fn main() {
-    labview::process::launch();
+
+    SimpleLogger::new().init().unwrap();
+
+    let id = labview::process::launch();
+
+    debug!("Process launched at {}", id);
 
     let processes = labview::process::find_instances();
 
     for (pid, name) in processes {
         println!("[{}] {}", pid, name);
     }
+
+    let monitor = labview::process::ProcessMonitor::start(id);
+
+    std::thread::sleep(std::time::Duration::from_millis(10000));
+
+    print!("{}", monitor.check_process_stopped());
+
+    monitor.stop();
+
+
+
 }
