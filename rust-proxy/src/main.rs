b7892f2ab058cc1d4c2f574aaa928eaccef4f236
James McNally <james@wiresmithtech.com>
2021-01-24 20:50:44 +0000

    Add Windows Install Detection
    Added logic for install detection. Currently supports 64 bit windows targets only.

--- rust-proxy/src/main.rs
+++ rust-proxy/src/main.rs
@@ -1,10 +1,13 @@
 mod labview;
 
+use labview::detect_installations;
 use simple_logger::SimpleLogger;
 use std::path::PathBuf;
 
 fn main() {
     SimpleLogger::new().init().unwrap();
+
+    detect_installations();
 
     let monitor = labview::process::MonitoredProcess::start(PathBuf::from(
         "C:\\Program Files (x86)\\National Instruments\\LabVIEW 2011\\LabVIEW.exe",
