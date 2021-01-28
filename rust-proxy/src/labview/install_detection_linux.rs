351b1bd3bbe0ed24921220ea59640b9144c77a32
James McNally <james@wiresmithtech.com>
2021-01-28 17:14:52 +0000

    Match cargo fmt output

--- rust-proxy/src/labview/install_detection_linux.rs
+++ rust-proxy/src/labview/install_detection_linux.rs
@@ -1,6 +1,4 @@
-
-
-use super::installs::{SystemLabviewInstalls, LabviewInstallError};
+use super::installs::{LabviewInstallError, SystemLabviewInstalls};
 
 /// Scan the system for LabVIEW installs and return their details.
 pub fn detect_installations() -> Result<SystemLabviewInstalls, LabviewInstallError> {
