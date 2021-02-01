355d9fd898a680b976cfb07635ca5cc9165411a2
James McNally <james@wiresmithtech.com>
2021-02-01 21:09:49 +0000

    Add Basic Command Line Parameters
    * Added code structure for handling parameters.
    * Added verbose mode support using log and debug levels.

---
+++ rust-proxy/src/cli.rs
@@ -0,0 +1,80 @@
+//! Contains the CLI API Defintion.
+//!
+use clap::{App, AppSettings, Arg};
+use std::iter::IntoIterator;
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+pub fn get_app() -> clap::App<'static, 'static> {
+    App::new("G CLI")
+        .version(VERSION)
+        .about("Connects a LabVIEW app to the command line.")
+        .arg(
+            Arg::with_name("verbose mode")
+                .short("v")
+                .long("verbose")
+                .help("Prints additional details for debugging"),
+        )
+        .setting(AppSettings::TrailingVarArg)
+        .arg(Arg::with_name("app to run").multiple(true).required(true))
+}
+
+pub fn program_arguments<T: IntoIterator<Item = String>>(main_args: T) -> Vec<String> {
+    let args_iter = main_args.into_iter();
+
+    args_iter.skip_while(|s| s != "--").skip(1).collect()
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn get_item_to_run() {
+        let args = vec![
+            String::from("g-cli"),
+            String::from("test.vi"),
+            String::from("--"),
+            String::from("test1"),
+            String::from("-t"),
+            String::from("test2"),
+        ];
+
+        let processed = get_app().get_matches_from_safe(args).unwrap();
+
+        assert_eq!(processed.value_of("app_to_run"), Some("test.vi"));
+    }
+
+    #[test]
+    fn get_program_arguments() {
+        let args = vec![
+            String::from("g-cli"),
+            String::from("test.vi"),
+            String::from("--"),
+            String::from("test1"),
+            String::from("-t"),
+            String::from("test2"),
+        ];
+
+        let processed = program_arguments(args);
+
+        assert_eq!(
+            processed,
+            vec![
+                String::from("test1"),
+                String::from("-t"),
+                String::from("test2")
+            ]
+        );
+    }
+
+    #[test]
+    fn get_program_arguments_empty() {
+        let args = vec![String::from("g-cli"), String::from("test.vi")];
+
+        let processed = program_arguments(args);
+
+        assert_eq!(processed, Vec::<String>::new());
+    }
+}
--- rust-proxy/src/main.rs
+++ rust-proxy/src/main.rs
@@ -1,11 +1,29 @@
+mod cli;
 mod labview;
 
 use labview::detect_installations;
+use log::{debug, LevelFilter};
 use simple_logger::SimpleLogger;
 use std::path::PathBuf;
 
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
 fn main() {
-    SimpleLogger::new().init().unwrap();
+    let args = cli::get_app().get_matches();
+    let program_args = cli::program_arguments(std::env::args());
+
+    let log_level = if args.is_present("verbose mode") {
+        LevelFilter::Debug
+    } else {
+        LevelFilter::Info
+    };
+
+    SimpleLogger::new().with_level(log_level).init().unwrap();
+
+    debug!("G CLI Started - Verbose Mode");
+    debug!("Version {}", VERSION);
+    debug!("G CLI Arguments: TBC");
+    debug!("Arguments passed to LabVIEW: {}", program_args.join(" "));
 
     detect_installations();
 
