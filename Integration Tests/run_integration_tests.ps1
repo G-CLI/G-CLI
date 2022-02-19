83f52dc626dbe55bdd6f38ac2f2b91ba16db7637
James McNally <james@wiresmithtech.com>
2022-02-19 07:34:20 +0000

    Move Integration Tests to Powershell
    As this is how most CI tools call it it makes more sense.
    
    I also suspect error detection will be easier

---
+++ Integration Tests/run_integration_tests.ps1
@@ -0,0 +1,81 @@
+
+
+$common_params='-v'
+$delay_between_tests=3
+$cli_cmd= $args[0] + 'g-cli'
+echo $cli_cmd
+
+& "$cli_cmd" $common_params "Echo Parameters.vi" -- "Param 1" "Param 2" | find "Param 1	Param 2"
+if(!$?) { 
+  echo "Echo Parameters VI Failed"
+  Exit 1
+ }
+Start-Sleep -s $delay_between_tests
+
+& "$cli_cmd" $common_params "Echo CWD.vi" | find $pwd
+if(!$?) { 
+  echo "Echo CWD VI Failed"
+  Exit 1
+ }
+Start-Sleep -s $delay_between_tests
+
+& "$cli_cmd" $common_params "Generate Large Output.vi" -- 10000
+if(!$?) { 
+  echo "Large Output VI Failed"
+  Exit 1
+ }
+Start-Sleep -s $delay_between_tests
+
+
+& "$cli_cmd" $common_params "Quit With Parameter Code.vi" -- 10000
+echo "Exit Code $LastExitCode"
+if ($LastExitCode -ne 10000) {
+  echo "Quit with Code VI Failed"
+  Exit 1
+}
+Start-Sleep -s $delay_between_tests
+
+
+& "$cli_cmd" $common_params "Quit With Parameter Code.vi" -- -10000
+echo "Exit Code $LastExitCode"
+if ($LastExitCode -ne -10000) {
+  echo "Quit with Negative Code VI Failed"
+  Exit 1
+}
+Start-Sleep -s $delay_between_tests
+
+
+& "$cli_cmd" $common_params ".\exes\Echo CLI.exe" -- "Param 1" "Param 2" | find "Param 1	Param 2"
+if(!$?) { 
+  echo "Echo Parameters EXE Failed"
+  Exit 1
+ }
+Start-Sleep -s $delay_between_tests 
+
+
+& "$cli_cmd" $common_params ".\exes\Echo CWD.exe" | find $pwd
+if(!$?) { 
+  echo "Echo CWD EXE Failed"
+  Exit 1
+ }
+Start-Sleep -s $delay_between_tests
+
+
+& "$cli_cmd" $common_params ".\exes\LargeOutput.exe" -- 10000
+if(!$?) { 
+  echo "Large Output EXE Failed"
+  Exit 1
+ }
+Start-Sleep -s $delay_between_tests
+
+& "$cli_cmd" $common_params ".\exes\QuitWithCode.exe" -- 10000
+if ($LastExitCode -ne 10000) {
+  Echo "Quit with Code EXE Failed"
+  Exit 1
+}
+Start-Sleep -s $delay_between_tests
+
+
+echo "All Tests Completed Successfully"
+Exit 0
+
