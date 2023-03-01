5b2161d2627e428f51397258c209dc3339c3de6e
James McNally <james@wiresmithtech.com>
2023-03-01 10:42:56 +0000

    Update Integration Tests for Linux Compatibility
    * Fix recommendations to avoid aliases.
    * Use target exit code in 0-255 range supported on Linux
    * Make negative exit code test windows specific.

--- run_integration_tests.ps1
+++ run_integration_tests.ps1
@@ -2,121 +2,122 @@
 $ErrorActionPreference = 'stop'
 
 
-$delay_between_tests=3
-$cli_cmd= $args[0] + 'g-cli'
-echo $cli_cmd 
+$delay_between_tests = 3
+$cli_cmd = $args[0] + 'g-cli'
+Write-Output $cli_cmd 
 
-echo "Echo Parameters"
-$matches = & "$cli_cmd" "LabVIEW Source/integration-tests/Echo Parameters.vi" -- "Param 1" "Param 2" | %{$_.Trim() -eq "Param 1	Param 2" }
-if(!$matches) { 
-  echo "Echo Parameters VI Failed"
+Write-Output "Echo Parameters"
+$output_matches = & "$cli_cmd" "LabVIEW Source/integration-tests/Echo Parameters.vi" -- "Param 1" "Param 2" | % { $_.Trim() -eq "Param 1	Param 2" }
+if (!$output_matches) { 
+  Write-Output "Echo Parameters VI Failed"
   Exit 1
- }
+}
 Start-Sleep -s $delay_between_tests
 
-echo "Echo CWD"
-$matches = & "$cli_cmd" "LabVIEW Source/integration-tests/Echo CWD.vi"  | %{$_.Trim() -eq $pwd.Path }
-if(!$matches) { 
-  echo "Echo CWD VI Failed"
+Write-Output "Echo CWD"
+$output_matches = & "$cli_cmd" "LabVIEW Source/integration-tests/Echo CWD.vi"  | % { $_.Trim() -eq $pwd.Path }
+if (!$output_matches) { 
+  Write-Output "Echo CWD VI Failed"
   Exit 1
- }
+}
 Start-Sleep -s $delay_between_tests
 
-echo "lvlibp Echo CWD"
-##$matches = & "$cli_cmd" "Builds/Tests.lvlibp/cwd.vi" | %{$_.Trim() -eq $pwd.Path }
-##if(!$matches) { 
-##  echo "Echo CWD VI Failed"
+Write-Output "lvlibp Echo CWD"
+##$output_matches = & "$cli_cmd" "Builds/Tests.lvlibp/cwd.vi" | %{$_.Trim() -eq $pwd.Path }
+##if(!$output_matches) { 
+##  Write-Output "Echo CWD VI Failed"
 ##  Exit 1
 ## }
 ##Start-Sleep -s $delay_between_tests
-echo "WARNING: Skipping packed library test due to build issue"
+Write-Output "WARNING: Skipping packed library test due to build issue"
 
-echo "Large Output"
+Write-Output "Large Output"
 & "$cli_cmd" $common_params "LabVIEW Source/integration-tests/Generate Large Output.vi" -- 10000
-if(!$?) { 
-  echo "Large Output VI Failed"
+if (!$?) { 
+  Write-Output "Large Output VI Failed"
   Exit 1
- }
+}
 Start-Sleep -s $delay_between_tests
 
-echo "Large Output Error"
+Write-Output "Large Output Error"
 $ErrorActionPreference = 'continue'
 $output = & "$cli_cmd" $common_params "LabVIEW Source/integration-tests/Generate Large Error.vi" -- 10000 2>&1
-$errors = $output | ?{$_.gettype().Name -eq "ErrorRecord"}
+$errors = $output | Where-Object { $_.gettype().Name -eq "ErrorRecord" }
 Write-Host "STDERR"
 Write-Host $errors
-if(!$errors) { 
-  echo "Nothing in Error Output"
+if (!$errors) { 
+  Write-Output "Nothing in Error Output"
   Exit 1
- }
+}
 Start-Sleep -s $delay_between_tests
 $ErrorActionPreference = 'stop'
 
 
-& "$cli_cmd" $common_params "LabVIEW Source/integration-tests/Quit With Parameter Code.vi" -- 10000
-echo "Exit Code $LastExitCode"
-if ($LastExitCode -ne 10000) {
-  echo "Quit with Code VI Failed"
+& "$cli_cmd" $common_params "LabVIEW Source/integration-tests/Quit With Parameter Code.vi" -- 100
+Write-Output "Exit Code $LastExitCode"
+if ($LastExitCode -ne 100) {
+  Write-Output "Quit with Code VI Failed"
   Exit 1
 }
 Start-Sleep -s $delay_between_tests
 
+if ($IsWindows) {
+  & "$cli_cmd" $common_params "LabVIEW Source/integration-tests/Quit With Parameter Code.vi" -- -10000
+  Write-Output "Exit Code $LastExitCode"
+  if ($LastExitCode -ne -10000) {
+    Write-Output "Quit with Negative Code VI Failed"
+    Exit 1
+  }
+  Start-Sleep -s $delay_between_tests
+}
 
-& "$cli_cmd" $common_params "LabVIEW Source/integration-tests/Quit With Parameter Code.vi" -- -10000
-echo "Exit Code $LastExitCode"
-if ($LastExitCode -ne -10000) {
-  echo "Quit with Negative Code VI Failed"
+
+$output_matches = & "$cli_cmd" $common_params "LabVIEW Source/integration-tests/Check Unicode Response.vi" -- "HÜll°"  | ForEach-Object { $_.Trim() -eq "HÜll°" }
+if (!$output_matches) { 
+  Write-Output "Non-Ascii in Input/Output Failed"
   Exit 1
 }
 Start-Sleep -s $delay_between_tests
 
-
-$matches = & "$cli_cmd" $common_params "LabVIEW Source/integration-tests/Check Unicode Response.vi" -- "HÜll°"  | %{$_.Trim() -eq "HÜll°" }
-if(!$matches) { 
-  echo "Non-Ascii in Input/Output Failed"
+$output_matches = & "$cli_cmd" $common_params "LabVIEW Source/integration-tests/Check Unicode Response HÜll°.vi" -- "HÜll°"   | ForEach-Object { $_.Trim() -eq "HÜll°" }
+if (!$output_matches) { 
+  Write-Output "Non-Ascii in Name Failed"
   Exit 1
- }
-Start-Sleep -s $delay_between_tests
-
-$matches = & "$cli_cmd" $common_params "LabVIEW Source/integration-tests/Check Unicode Response HÜll°.vi" -- "HÜll°"   | %{$_.Trim() -eq "HÜll°" }
-if(!$matches) { 
-  echo "Non-Ascii in Name Failed"
-  Exit 1
- }
+}
  
 Start-Sleep -s $delay_between_tests
-$matches = & "$cli_cmd" $common_params "Builds\Echo CLI.exe" -- "Param 1" "Param 2" | %{$_.Trim() -eq "Param 1	Param 2" }
-if(!$matches) { 
-  echo "Echo Parameters EXE Failed"
+$output_matches = & "$cli_cmd" $common_params "Builds\Echo CLI.exe" -- "Param 1" "Param 2" | ForEach-Object { $_.Trim() -eq "Param 1	Param 2" }
+if (!$output_matches) { 
+  Write-Output "Echo Parameters EXE Failed"
   Exit 1
- }
+}
 Start-Sleep -s $delay_between_tests 
 
 
 
-$matches = & "$cli_cmd" $common_params "Builds\Echo CWD.exe" | %{$_.Trim() -eq $pwd.Path }
-if(!$matches) { 
-  echo "Echo CWD EXE Failed"
-  Exit 1
- }
-Start-Sleep -s $delay_between_tests
-
-
-& "$cli_cmd" $common_params "Builds\LargeOutput.exe" -- 10000
-if(!$?) { 
-  echo "Large Output EXE Failed"
-  Exit 1
- }
-Start-Sleep -s $delay_between_tests
-
-& "$cli_cmd" $common_params "Builds\QuitWithCode.exe" -- 10000
-if ($LastExitCode -ne 10000) {
-  Echo "Quit with Code EXE Failed"
+$output_matches = & "$cli_cmd" $common_params "Builds\Echo CWD.exe" | ForEach-Object { $_.Trim() -eq $pwd.Path }
+if (!$output_matches) { 
+  Write-Output "Echo CWD EXE Failed"
   Exit 1
 }
 Start-Sleep -s $delay_between_tests
 
 
-echo "All Tests Completed Successfully"
+& "$cli_cmd" $common_params "Builds\LargeOutput.exe" -- 10000
+if (!$?) { 
+  Write-Output "Large Output EXE Failed"
+  Exit 1
+}
+Start-Sleep -s $delay_between_tests
+
+& "$cli_cmd" $common_params "Builds\QuitWithCode.exe" -- 100
+if ($LastExitCode -ne 100) {
+  Write-Output "Quit with Code EXE Failed"
+  Exit 1
+}
+Start-Sleep -s $delay_between_tests
+
+
+Write-Output "All Tests Completed Successfully"
 Exit 0
 
